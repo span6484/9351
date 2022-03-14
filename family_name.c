3d35f26b7d283723882b58c2f670ea2b1370670d
stephenpsq <stephenpsq@gmail.com>
2022-03-14 08:34:40 +0800

    update family_name
    change all family_name malloc to palloc

--- family_name.c
+++ family_name.c
@@ -1,44 +1,36 @@
-//
-// Created by shao on 2022/3/10.
-//
+family(PG_FUNCTION_ARGS)
+        {
+                int i;
+                int comma_num = 0;          //; appear times
+                int comma_index = -1;        // , index
+                char* family_name;
+                int family_name_len;
+                int space_after_commaTime = 0;
+                char *pname;
+                PersonName    *a = (PersonName *) PG_GETARG_POINTER(0);
+                pname = psprintf("%s", a->pname);	//格式化输出
+                for (i = 0; i < strlen(pname); i++) {
+                    if (!isspace(pname[i])) {
+                        space_after_commaTime = 0;
+                    }
+                    if (isspace(pname[i]) && (i == comma_index+1)) {
+                        space_after_commaTime += 1;
+                    }
+                    if (pname[i] == ',') {
 
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
+                        comma_index = i;
+                        comma_num += 1;
+                        space_after_commaTime = 0;
+                    }
 
+                }
+                family_name_len = comma_index;      //'a\0'
+                //// elog(NOTICE, "comma index palloc size is %d", comma_index);
+                //// elog(NOTICE, "family name len is %d",family_name_len);
+                //// elog(NOTICE, "family name palloc size is %d", family_name_len+1);
+                family_name = palloc(sizeof (char) * family_name_len+1);
+                strncpy(family_name,pname, comma_index);
+                family_name[comma_index] = '\0';
+                PG_RETURN_CSTRING(family_name);			//return 一个字符串
 
-int main(void) {
-    char* pname = "abc,def";
-    int i = 0;
-    int comma_num = 0;          //; appear times
-    int comma_index = 0;        // , index
-    char* family_name;
-    int family_name_len;
-    int space_after_commaTime = 0;
-
-    for (i = 0; i < strlen(pname); i++) {
-
-        if (!isspace(pname[i])) {
-            space_after_commaTime = 0;
         }
-
-        if (isspace(pname[i]) && (i == comma_index+1)) {
-            space_after_commaTime += 1;
-        }
-        if (pname[i] == ',') {
-            comma_index = i;
-            comma_num += 1;
-            space_after_commaTime = 0;
-        }
-
-    }
-
-
-    family_name_len = comma_index + 1;      //'a\0'
-    family_name = malloc(sizeof (char) * family_name_len);
-    strncpy(family_name,pname, comma_index);
-    family_name[comma_index] = '\0';
-    free(family_name);
-    return condition;
-}
--- pname.c
+++ pname.c
@@ -342,42 +342,39 @@ PG_FUNCTION_INFO_V1(family);
 Datum
 family(PG_FUNCTION_ARGS)
 {
-    char *result;
-    PersonName    *a = (PersonName *) PG_GETARG_POINTER(0);
-    //to do
-
-    int i = 0;
+    int i;
     int comma_num = 0;          //; appear times
-    int comma_index = 0;        // , index
+    int comma_index = -1;        // , index
     char* family_name;
     int family_name_len;
     int space_after_commaTime = 0;
-
-    for (i = 0; i < strlen(a->pname); i++) {
-
-        if (!isspace(a->pname[i])) {
+    char *pname;
+    PersonName    *a = (PersonName *) PG_GETARG_POINTER(0);
+    pname = psprintf("%s", a->pname);	//格式化输出
+    for (i = 0; i < strlen(pname); i++) {
+        if (!isspace(pname[i])) {
             space_after_commaTime = 0;
         }
-
-        if (isspace(a->pname[i]) && (i == comma_index+1)) {
+        if (isspace(pname[i]) && (i == comma_index+1)) {
             space_after_commaTime += 1;
         }
-        if (a->pname[i] == ',') {
+        if (pname[i] == ',') {
+
             comma_index = i;
             comma_num += 1;
             space_after_commaTime = 0;
         }
 
     }
-    family_name_len = comma_index + 1;      //'a\0'
-    family_name = malloc(sizeof (char) * family_name_len+1);
-    strncpy(family_name,a->pname, comma_index);
+    family_name_len = comma_index;      //'a\0'
+    //// elog(NOTICE, "comma index palloc size is %d", comma_index);
+    //// elog(NOTICE, "family name len is %d",family_name_len);
+    //// elog(NOTICE, "family name palloc size is %d", family_name_len+1);
+    family_name = palloc(sizeof (char) * family_name_len+1);
+    strncpy(family_name,pname, comma_index);
     family_name[comma_index] = '\0';
+    PG_RETURN_CSTRING(family_name);			//return 一个字符串
 
-    // 把family name取出来返回, dont change
-    result = psprintf("%s", family_name);	//格式化输出
-    PG_RETURN_CSTRING(result);			//return 一个字符串
-    free(family_name);
 }
 
 
